97ae87d88e5b09aecb9f635dc678c28f1daad77b
スツゼル <23124682+stuzer05@users.noreply.github.com>
2022-12-30 12:12:09 +0200

    Fix crash
    class MediaWiki\ResourceLoader\Module is not available

--- includes/SearchnamespaceTokenModule.php
+++ includes/SearchnamespaceTokenModule.php
@@ -2,54 +2,55 @@
 
 namespace AdvancedSearch;
 
-use MediaWiki\ResourceLoader as RL;
-use MediaWiki\ResourceLoader\ResourceLoader;
+use ResourceLoaderContext;
+use ResourceLoaderModule;
+use ResourceLoaderWikiModule;
 use Xml;
 
 /**
  * ResourceLoader module providing the users "searchnamespace" token.
  */
-class SearchnamespaceTokenModule extends RL\Module {
+class SearchnamespaceTokenModule extends ResourceLoaderWikiModule {
+
+        /**
+         * @var int
+         */
+        protected $origin = self::ORIGIN_CORE_INDIVIDUAL;
+
+        /**
+         * @var string[]
+         */
+        protected $targets = [ 'desktop', 'mobile' ];
+
+        /**
+         * @param ResourceLoaderContext $context
+         *
+         * @return string JavaScript code
+         */
+        public function getScript( ResourceLoaderContext $context ) {
+                $user = $context->getUserObj();
+                // Use FILTER_NOMIN annotation to prevent needless minification and caching (T84960).
+                return ResourceLoader::FILTER_NOMIN .
+                        Xml::encodeJsCall(
+                                'mw.user.tokens.set',
+                                [ 'searchnamespaceToken', $user->getEditToken( 'searchnamespace' ) ],
+                                (bool)ResourceLoader::inDebugMode()
+                        );
+        }
+
+        /**
+         * @return bool
+         */
+        public function supportsURLLoading() {
+                return false;
+        }
 
 	/**
-	 * @var int
-	 */
-	protected $origin = self::ORIGIN_CORE_INDIVIDUAL;
-
-	/**
-	 * @var string[]
-	 */
-	protected $targets = [ 'desktop', 'mobile' ];
-
-	/**
-	 * @param RL\Context $context
-	 *
-	 * @return string JavaScript code
-	 */
-	public function getScript( RL\Context $context ) {
-		$user = $context->getUserObj();
-		// Use FILTER_NOMIN annotation to prevent needless minification and caching (T84960).
-		return ResourceLoader::FILTER_NOMIN .
-			Xml::encodeJsCall(
-				'mw.user.tokens.set',
-				[ 'searchnamespaceToken', $user->getEditToken( 'searchnamespace' ) ],
-				(bool)ResourceLoader::inDebugMode()
-			);
-	}
-
-	/**
-	 * @return bool
-	 */
-	public function supportsURLLoading() {
-		return false;
-	}
-
-	/**
-	 * @return string
-	 */
-	public function getGroup() {
-		// Private modules can not be loaded as a dependency, only via OutputPage::addModules().
-		return 'private';
-	}
+         * @return string
+         */
+        public function getGroup() {
+                // Private modules can not be loaded as a dependency, only via OutputPage::addModules().
+                return 'private';
+        }
 
 }
